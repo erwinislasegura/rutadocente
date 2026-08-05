6179207a87709a0289ba914b6e45cfac12f268fc
erwinislasegura <erwin.2785@gmail.com>
2026-08-05 02:28:05 -0400

    Integrate PHP public site with teacher login

--- app/Controllers/PublicController.php
+++ app/Controllers/PublicController.php
@@ -1,5 +1,14 @@
 <?php
 namespace App\Controllers;
 use App\Core\Controller;
-class PublicController extends Controller { function home():void{$this->view('public/home',['title'=>'Inicio']);} }
-
+class PublicController extends Controller {
+ function home():void{$this->publicPage('home');}
+ function asignaturas():void{$this->publicPage('asignaturas');}
+ function portafolio():void{$this->publicPage('portafolio');}
+ function clasesAsincronicas():void{$this->publicPage('clases-asincronicas');}
+ function correctoresIa():void{$this->publicPage('correctores-ia');}
+ function tabuladores():void{$this->publicPage('tabuladores');}
+ function recursos():void{$this->publicPage('recursos');}
+ function contacto():void{$this->publicPage('contacto');}
+ function preguntasFrecuentes():void{$this->publicPage('preguntas-frecuentes');}
+}
