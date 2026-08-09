d50f1be1105757ed58d31a9bbfc041754f14f391
erwinislasegura <erwin.2785@gmail.com>
2026-08-09 09:35:50 -0400

    Usar icono oficial de WhatsApp

--- app/Views/public/home.php
+++ app/Views/public/home.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="description" content="Recursos y acompañamiento para docentes de Chile">
     <title>Inicio | Ruta Docente 2026</title>
-    <link rel="stylesheet" href="<?=url('/assets/css/site.css?v=20260808-sello1')?>">
+    <link rel="stylesheet" href="<?=url('/assets/css/site.css?v=20260809-whatsapp1')?>">
     <link rel="icon" href="<?=url('/assets/img/logo-ruta-docente.png')?>">
 </head>
 <body>
--- app/Views/public/tests.php
+++ app/Views/public/tests.php
@@ -5,7 +5,7 @@
  <meta name="viewport" content="width=device-width,initial-scale=1">
  <meta name="description" content="Tests y evaluaciones para fortalecer la preparación docente 2026">
  <title>Tus test | Ruta Docente 2026</title>
- <link rel="stylesheet" href="<?=url('/assets/css/site.css?v=20260808-legibilidad1')?>">
+ <link rel="stylesheet" href="<?=url('/assets/css/site.css?v=20260809-whatsapp1')?>">
  <link rel="icon" href="<?=url('/assets/img/logo-ruta-docente.png')?>">
 </head>
 <body class="tests-public-page">
